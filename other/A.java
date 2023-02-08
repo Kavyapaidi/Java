a6bc83bc4092c87c98a40a2a17c26285e7a5eeba
Paidi Kavyasri <86728151+Kavyapaidi@users.noreply.github.com>
2023-02-08 15:23:32 +0530

    Add files via upload

---
+++ other/A.java
@@ -0,0 +1,18 @@
+package other;
+
+public class A {
+    public int marks=6;
+    public void show()
+    {
+        System.out.println("show() in class A");
+    }
+}
+class Launch
+{
+    public void abc()
+    {
+        A obj=new A();
+
+    }
+}
+
---
+++ other/tools/AdvCalci.java
@@ -0,0 +1,13 @@
+package other.tools;
+
+//other.tools.AdvCalci is sub class, other.tools.Calci is super class
+public class AdvCalci extends Calci {
+    public int multi(int n1,int n2)
+    {
+        return n1*n2;
+    }
+    public int div(int n1,int n2)
+    {
+        return n1/n2;
+    }
+}
